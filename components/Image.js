5bcc35b5f73c991fdce745953d2ffc2b8dba77a4
mr0zek <szymon.szylhabel@gmail.com>
2022-03-03 00:25:32 +0100

    basepath

--- components/Image.js
+++ components/Image.js
@@ -3,6 +3,6 @@ import getConfig from 'next/config'
 const { publicRuntimeConfig } = getConfig()
 
 // eslint-disable-next-line jsx-a11y/alt-text
-const Image = ({ src, ...rest }) => <img src={`${publicRuntimeConfig.basePath}/` + src} {...rest} />
+const Image = ({ src, ...rest }) => <img src={`${publicRuntimeConfig.basePath}` + src} {...rest} />
 
 export default Image
